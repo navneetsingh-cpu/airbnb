605a303c0b3dcc6421f8036d8b83d79d81e538bf
Navneet <hunnyhunnyhunny@gmail.com>
2024-05-20 11:52:49 -0400

    Prisma init

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -4,21 +4,43 @@ import FormInput from "@/components/form/FormInput";
 import { SubmitButton } from "@/components/form/Buttons";
 import FormContainer from "@/components/form/FormContainer";
 import { profileSchema } from "@/utils/schemas";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import db from "@/utils/db";
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error("Please login to create a profile");
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return { message: "Profile Created" };
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
-    console.log(error);
-    return { message: "there was an error..." };
+    return {
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
   }
+  redirect("/");
 };
+
 function CreateProfile() {
   return (
     <section>
